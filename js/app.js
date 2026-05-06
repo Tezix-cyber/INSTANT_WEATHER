2e09a51c9e62e62bfb4bc3156df81cb7eb7f26e8
Tezix <nathan.duvalytb@gmail.com>
2026-05-06 16:39:04 +0200

    Remove async function that fetches commune data

--- js/app.js
+++ js/app.js
@@ -1,6 +1,1 @@
-async function oui(CodePostal) {
-    const response = await fetch("https://geo.api.gouv.fr/communes?codePostal="+CodePostal)
-    const data = await response.json() 
-    console.log(data)
-}
-oui(50440)
+
